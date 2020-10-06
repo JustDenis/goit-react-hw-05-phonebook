97285a7a2fb5a3df559eb77d5fe2d160545bd1cf
JustDenis <denisdenis2343@gmail.com>
2020-10-06 15:05:29 +0300

    Updates

---
+++ precache-manifest.a9e176b44e6dd5d354aad231b81761e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00456097a3326ca960e639ae229e238",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "2e7ce5ef0abbe03abd05",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.a9b0118f.chunk.css"
+  },
+  {
+    "revision": "f7cde1a9f197224d9888",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.2e2d8c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.2e2d8c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7ce5ef0abbe03abd05",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.314bc9ee.chunk.js"
+  },
+  {
+    "revision": "3f441844d3ba7dba1f3a",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.61bf2916.js"
+  }
+]);
